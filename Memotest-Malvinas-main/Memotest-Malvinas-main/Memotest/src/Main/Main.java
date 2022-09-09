155c39ff89497425f0d37379e608995dc66122b3
sosita <101130412+maxisosaa@users.noreply.github.com>
2022-09-09 14:17:54 -0300

    Add files via upload

---
+++ Memotest-Malvinas-main/Memotest-Malvinas-main/Memotest/src/Main/Main.java
@@ -0,0 +1,17 @@
+package Main;
+
+import java.security.Principal;
+
+public class Main {
+
+   
+    public static void main(String[] args) {
+        /*
+        Principal start = new Principal() {
+            public String getName() {
+                throw new UnsupportedOperationException("Not supported yet."); 
+            }
+        };*/
+       
+    }
+}
